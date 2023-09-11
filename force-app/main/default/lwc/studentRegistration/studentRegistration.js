f702daf080fe73cf23a0eace983a3726de603dd7
Kishorhus <mudholkishor@gmail.com>
2023-09-11 16:00:26 +0530

    This is simple LWC component create for student registration

---
+++ force-app/main/default/lwc/studentRegistration/studentRegistration.js
@@ -0,0 +1,27 @@
+import { LightningElement } from 'lwc';
+
+
+
+/*import { LightningElement } from 'lwc';
+*/
+export default class Student_Registration extends LightningElement {
+    firstName = '';
+    lastName = '';
+    email = '';
+
+    handleFirstNameChange(event) {
+        this.firstName = event.target.value;
+    }
+    
+    handleLastNameChange(event) {
+        this.lastName = event.target.value;
+    }
+
+    handleEmailChange(event) {
+        this.email = event.target.value;
+    }
+
+    handleSubmit() {
+        // Add logic to handle form submission here
+    }
+}
