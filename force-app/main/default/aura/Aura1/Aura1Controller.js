876843d6bf4467c0a9a1bad21aeef8b0f9d6abf6
Kishorhus <mudholkishor@gmail.com>
2023-09-13 20:10:03 +0530

    Aura Demo component created

--- force-app/main/default/aura/Aura1/Aura1Controller.js
+++ force-app/main/default/aura/Aura1/Aura1Controller.js
@@ -1,5 +1,19 @@
+/*
 ({
     myAction : function(component, event, helper) {
 
+    }
+})
+
+*/
+
+({
+    saveUserInfo: function(component, event, helper) {
+        var firstName = component.get("v.firstName");
+        var lastName = component.get("v.lastName");
+        
+        
+        console.log("First Name: " + firstName);
+        console.log("Last Name: " + lastName);
     }
 })
